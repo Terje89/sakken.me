ab55c6759283db3aa279515dc7808825314e7c65
Terje89 <terje@sakariassen.me>
2020-10-20 19:53:04 +0200

    Update blog-post.js

--- web/src/components/blog-post.js
+++ web/src/components/blog-post.js
@@ -12,19 +12,6 @@ function BlogPost (props) {
   const {_rawBody, authors, categories, title, mainImage, publishedAt} = props
   return (
     <article className={styles.root}>
-      {mainImage && mainImage.asset && (
-        <div className={styles.mainImage}>
-          <img
-            src={imageUrlFor(buildImageObj(mainImage))
-              .width(1200)
-              .height(Math.floor((9 / 16) * 1200))
-              .fit('crop')
-              .auto('format')
-              .url()}
-            alt={mainImage.alt}
-          />
-        </div>
-      )}
       <Container>
         <div className={styles.grid}>
           <div className={styles.mainContent}>
