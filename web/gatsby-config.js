da155d9e5297360734bb3aad63eb94254ece1d8f
Terje89 <terje@sakariassen.me>
2020-10-21 19:40:01 +0200

    Update gatsby-config.js

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -8,8 +8,35 @@ const clientConfig = require('./client-config')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  siteMetadata: {
+    siteUrl: `https://sakken.me`,
+  },
   plugins: [
     'gatsby-plugin-postcss',
+    'gatsby-plugin-sitemap',
+    {
+    resolve: "gatsby-plugin-google-tagmanager",
+    options: {
+      id: "GTM-TKJWB2S",
+
+      // Include GTM in development.
+      //
+      // Defaults to false meaning GTM will only be loaded in production.
+      includeInDevelopment: false,
+
+      // datalayer to be set before GTM is loaded
+      // should be an object or a function that is executed in the browser
+      //
+      // Defaults to null
+      defaultDataLayer: { platform: "gatsby" },
+
+      // Name of the event that is triggered
+      // on every Gatsby route change.
+      //
+      // Defaults to gatsby-route-change
+      routeChangeEventName: "Route",
+    },
+  },
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-sanity',
